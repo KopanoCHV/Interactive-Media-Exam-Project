8ec4225335b27366c3a8bcd448a33a3ca3a351c2
kopano Kgosimotswedi <kopanokopano174@gmail.com>
2026-06-05 18:17:23 +0200

    About Page Comments

--- AboutPage/AboutPageApp.js
+++ AboutPage/AboutPageApp.js
@@ -1,3 +1,5 @@
+//--------Loading Screen Logic--------------------
+
 let loader = document.querySelector("#page-load");
 
 window.addEventListener("load", (event) => {
@@ -5,6 +7,8 @@ window.addEventListener("load", (event) => {
     loader.style.display = "none";
 
 });
+
+//---------Image Modal Logic-----------------------------------
 
 const openImg = document.getElementById("openModal");
 const closeBtn = document.getElementById("closeModal");
@@ -17,6 +21,8 @@ openImg.addEventListener("click",(event) => {
 closeBtn.addEventListener("click", (event) => {
     modal.classList.remove("open");
 });
+
+//-----------Page Navigation Logic-------------------------------------------
 
 const homeNav = document.querySelector("#home-navigation");
 const portfolioNav = document.querySelector("#portfolio-navigation");
@@ -53,6 +59,8 @@ prevPageNav.addEventListener("click", (event) => {
     window.location.href = "../Project4/Project4.html"
 
 });
+
+//-----------Projects Sub-Navigation Logic----------------------------
 
 const controlBtn = document.querySelector("#modal-control");
 
@@ -99,6 +107,8 @@ Project4.addEventListener("click", (event) => {
 
 });
 
+//------------Navigation Highligh Logic-------------------------------------
+
 // Using window.location to get the path of the open page.
 
 const currentPage = window.location.pathname;
@@ -122,7 +132,7 @@ if(currentPage === '/AboutPage/AboutPage.html'){
     Project4.classList.remove("current");
 }
 
-// Dark mode logic
+//------------------------Dark mode logic--------------------------------------
 
 const switchBtn = document.querySelector("#switch");
 
